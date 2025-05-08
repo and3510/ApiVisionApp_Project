0bcccc43f617702e4d2c2e9f429ff038a549c234
anderson <and.dovale@gmail.com>
2025-05-08 01:30:20 +0000

    ajeitar o delete

--- functions/crud/delete_identidade.py
+++ functions/crud/delete_identidade.py
@@ -17,31 +17,31 @@ ssp_criminosos_db_dependency = Annotated[Session, Depends(get_ssp_criminosos_db)
 
 SspCriminososBase.metadata.create_all(bind=ssp_criminosos_engine)
 
-
-async def delete_identidade(cpf: str, db: ssp_criminosos_db_dependency):
+def delete_identidade(cpf: str, db: ssp_criminosos_db_dependency):
     # Verificar se a identidade existe
     identidade = db.query(models.Identidade).filter(models.Identidade.cpf == cpf).first()
     if not identidade:
         raise HTTPException(status_code=404, detail="Identidade não encontrada.")
-    
+
+    # Buscar a ficha criminal associada
     ficha = db.query(models.FichaCriminal).filter(models.FichaCriminal.cpf == cpf).first()
 
-    
-    crimes = db.query(models.Crime).filter(models.Crime.id_ficha == ficha.id_ficha).all()
-
-    if crimes:
-        # Remover os crimes associados
-        for crime in crimes:
-            db.delete(crime)
-
     if ficha:
-        # Remover a ficha criminal associada
-        db.delete(ficha)
-    
-
+        # Remover os crimes associados à ficha criminal
+        try:
+            db.query(models.Crime).filter(models.Crime.id_ficha == ficha.id_ficha).delete()
+            # Remover a ficha criminal
+            db.delete(ficha)
+        except Exception as e:
+            db.rollback()
+            raise HTTPException(status_code=500, detail="Erro ao remover os dados associados: " + str(e))
 
     # Remover a identidade
-    db.delete(identidade)
-    db.commit()
+    try:
+        db.delete(identidade)
+        db.commit()
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=500, detail="Erro ao remover a identidade: " + str(e))
 
     return {"message": "Identidade e dados associados removidos com sucesso."}
