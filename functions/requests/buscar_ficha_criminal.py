97cb16c5397f31d40c5ff49e4529bedf55dfec9c
Anderson do Vale <127062559+and3510@users.noreply.github.com>
2025-06-03 15:05:47 -0300

    Update buscar_ficha_criminal.py

--- functions/requests/buscar_ficha_criminal.py
+++ functions/requests/buscar_ficha_criminal.py
@@ -1,10 +1,10 @@
 from fastapi import Depends, HTTPException
 from typing import Annotated
 from sqlalchemy.orm import Session
-from config.database import SspCriminososBase
-from functions.dependencias import get_ssp_criminosos_db
+from config.database import SspCriminososBase, SspUsuariosBase
+from functions.dependencias import get_ssp_criminosos_db, get_ssp_usuarios_db
 import config.models as models
-from config.database import ssp_criminosos_engine
+from config.database import ssp_criminosos_engine, ssp_usuarios_engine
 from uuid import uuid4
 from datetime import datetime
 import pytz 
@@ -14,9 +14,12 @@ ssp_criminosos_db_dependency = Annotated[Session, Depends(get_ssp_criminosos_db)
 
 SspCriminososBase.metadata.create_all(bind=ssp_criminosos_engine)
 
+ssp_usuarios_db_dependency = Annotated[Session, Depends(get_ssp_usuarios_db)]
+
+SspUsuariosBase.metadata.create_all(bind=ssp_usuarios_engine)
 
 
-def buscar_ficha_criminal(cpf: str, matricula: str, ficha_db: ssp_criminosos_db_dependency):
+def buscar_ficha_criminal(cpf: str, matricula: str, ficha_db: ssp_criminosos_db_dependency, user_db: ssp_usuarios_db_dependency):
     # Verificar se o CPF existe na tabela Identidade
     identidade = ficha_db.query(models.Identidade).filter(models.Identidade.cpf == cpf).first()
     if not identidade:
@@ -28,18 +31,22 @@ def buscar_ficha_criminal(cpf: str, matricula: str, ficha_db: ssp_criminosos_db_
     if ficha_criminal:
         crimes = ficha_db.query(models.Crime).filter(models.Crime.id_ficha == ficha_criminal.id_ficha).all()
 
+    usuario = user_db.query(models.Usuario).filter(models.Usuario.matricula == matricula).first()
+
     br_tz = pytz.timezone('America/Sao_Paulo')
+
     
-    log_entrada = models.Log_Entrada(
+    log_resultado_cpf = models.Log_Resultado_Cpf(
         id_entrada=str(uuid4()).replace("-", "")[:30],  # Gera um novo ID com no máximo 20 caracteres
         matricula=usuario.matricula,
         data_entrada_conta=datetime.now(br_tz).strftime("%H:%M:%S %d/%m/%Y"),  # Data atual
-        cpf=usuario.cpf,
-        id_usuario=usuario.id_usuario
+        cpf=ficha_criminal.cpf,
+        id_usuario=usuario.id_usuario,
+        id_ficha=ficha_criminal.id_ficha
     )
-    db.add(log_entrada)
+    db.add(log_resultado_cpf)
     db.commit()
-    db.refresh(log_entrada)
+    db.refresh(log_resultado_cpf)
 
     # Construir a resposta
     resposta = {
