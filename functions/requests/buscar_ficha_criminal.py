691f743bc2e9e5b30f5c687c613af0828f29e0c3
Anderson do Vale <127062559+and3510@users.noreply.github.com>
2025-06-03 14:55:59 -0300

    Update buscar_ficha_criminal.py

--- functions/requests/buscar_ficha_criminal.py
+++ functions/requests/buscar_ficha_criminal.py
@@ -5,6 +5,9 @@ from config.database import SspCriminososBase
 from functions.dependencias import get_ssp_criminosos_db
 import config.models as models
 from config.database import ssp_criminosos_engine
+from uuid import uuid4
+from datetime import datetime
+import pytz 
 
 
 ssp_criminosos_db_dependency = Annotated[Session, Depends(get_ssp_criminosos_db)]
@@ -13,7 +16,7 @@ SspCriminososBase.metadata.create_all(bind=ssp_criminosos_engine)
 
 
 
-def buscar_ficha_criminal(cpf: str, ficha_db: ssp_criminosos_db_dependency):
+def buscar_ficha_criminal(cpf: str, matricula: str, ficha_db: ssp_criminosos_db_dependency):
     # Verificar se o CPF existe na tabela Identidade
     identidade = ficha_db.query(models.Identidade).filter(models.Identidade.cpf == cpf).first()
     if not identidade:
@@ -24,6 +27,19 @@ def buscar_ficha_criminal(cpf: str, ficha_db: ssp_criminosos_db_dependency):
     crimes = []
     if ficha_criminal:
         crimes = ficha_db.query(models.Crime).filter(models.Crime.id_ficha == ficha_criminal.id_ficha).all()
+
+    br_tz = pytz.timezone('America/Sao_Paulo')
+    
+    log_entrada = models.Log_Entrada(
+        id_entrada=str(uuid4()).replace("-", "")[:30],  # Gera um novo ID com no máximo 20 caracteres
+        matricula=usuario.matricula,
+        data_entrada_conta=datetime.now(br_tz).strftime("%H:%M:%S %d/%m/%Y"),  # Data atual
+        cpf=usuario.cpf,
+        id_usuario=usuario.id_usuario
+    )
+    db.add(log_entrada)
+    db.commit()
+    db.refresh(log_entrada)
 
     # Construir a resposta
     resposta = {
